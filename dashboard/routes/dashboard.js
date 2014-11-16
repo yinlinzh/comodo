67f0dd3e0344c07c264565e5d480861e91406b27
Yinlin Zhang <yinlinzh@gmail.com>
2014-11-16 20:16:58 +0800

    add bower, try angular

--- dashboard/routes/dashboard.js
+++ dashboard/routes/dashboard.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
+//
+//router.get('/', function(req, res) {
+//  res.render('dashboard',{name: '<b>英超</b>', score: 263, goals: 129,
+//    content: '<b>jijijij</b>', title: 'dashboard', greeting: '{{ greeting.text }}',
+//    AngularJS: '{{"AngularJS"}}'
+//  });
+//});
 
-/* GET users listing. */
 router.get('/', function(req, res) {
-  res.render('dashboard',{name: '<b>英超</b>', score: 283, goals: 129,
-    content: '<b>jijijij</b>', title: 'dashboard'});
+  res.render('dashboard');
 });
 
 router.post('/', function(req, res) {
