f1d17ca07c126ea777b5df68ebf00227f15bf81b
Yinlin Zhang <yinlinzh@gmail.com>
2014-11-17 08:04:52 +0800

    change '{{' to '\{{' to avoid escaping by handlebars

--- dashboard/routes/dashboard.js
+++ dashboard/routes/dashboard.js
@@ -1,12 +1,5 @@
 var express = require('express');
 var router = express.Router();
-//
-//router.get('/', function(req, res) {
-//  res.render('dashboard',{name: '<b>英超</b>', score: 263, goals: 129,
-//    content: '<b>jijijij</b>', title: 'dashboard', greeting: '{{ greeting.text }}',
-//    AngularJS: '{{"AngularJS"}}'
-//  });
-//});
 
 router.get('/', function(req, res) {
   res.render('dashboard');
