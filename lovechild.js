cc5be2d7748c2e4760ace99bc606d065d30d3b22
Yinlin Zhang <yinlinzh@gmail.com>
2015-01-05 19:17:03 +0800

    update parent & child

--- lovechild.js
+++ lovechild.js
@@ -1,3 +1,6 @@
 process.on('message', function(msg) {
-    console.log('Parent said: ' + msg);
+    console.log('Parent said: ' , msg);
+//    process.exit(1);
+    process.send('I love you, too');
+    process.exit(1);
 });
--- parent.js
+++ parent.js
@@ -2,8 +2,9 @@ var path = require('path');
 var cp = require('child_process');
 var child = cp.fork(path.join(__dirname, 'lovechild.js'));
 
-child.on('exit', function(msg) {
-    console.log('Child said: ' + msg);
-});
+//child.on('message', function(msg) {
+//    console.log('Child said: ', msg);
+//    process.exit(13);
+//});
 
 child.send('I love you');
